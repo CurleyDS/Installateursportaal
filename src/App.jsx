f40b5b79ed5b24d9f7c652cd721cd0d895488adf
Dylan Schoop <dylanschoop@gmail.com>
2025-06-12 08:48:10 +0000

    Setup React App

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Installateursportaal</h1>
+      <div className="card">
+        <p>Virtuele omgeving voor het Installateursportaal</p>
+      </div>
+    </>
+  )
+}
+
+export default App
