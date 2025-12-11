b27ada6e2a6f182469eda6470653f5324a595237
Dylan Schoop <dylanschoop@gmail.com>
2025-12-11 10:08:53 +0000

    Testing features

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -6,7 +6,7 @@ function Test() {
     const [data, setData] = useState([]);
 
     useEffect(() => {
-        const fetchData = async () => {
+        const fetchCurrentRoomTemp = async () => {
             try {
                 const response = await fetch('https://hupie.northeurope.cloudapp.azure.com/hupie/query/?token=21129FCC24', {
                     method: 'POST',
@@ -30,14 +30,154 @@ function Test() {
                 setLoading(true);
             }
         }
+        
+        const fetchRoomTempSetpoint = async () => {
+            try {
+                const response = await fetch('https://hupie.northeurope.cloudapp.azure.com/hupie/query/?token=21129FCC24', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/sparql-query',
+                        'Accept': 'application/json'
+                    },
+                    body: `
+                        PREFIX hco: <https://www.tno.nl/building/ontology/heatpump-common-ontology#>
+                        PREFIX om: <http://www.ontology-of-units-of-measure.org/resource/om-2/>
+                        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+                        PREFIX saref: <https://saref.etsi.org/core/>
+                        PREFIX saref4bldg: <https://saref.etsi.org/saref4bldg/> 
+                        
+                        SELECT ?id ?value ?unit
+                        WHERE {
+                            ?heatPump rdf:type hco:HeatPump ;
+                                        saref:hasIdentifier ?id ;
+                                        saref:hasPropertyOfInterest ?temperatureSetpoint .
+                            ?temperatureSetpoint rdf:type saref:PropertyOfInterest ;
+                                                    saref:hasPropertyKind hco:TemperatureSetpoint .
+                            ?building rdf:type saref4bldg:Building ;
+                                        saref4bldg:contains ?heatPump ;
+                                        saref4bldg:hasSpace ?room .
+                            ?room rdf:type saref4bldg:BuildingSpace .
+                            ?heatPump saref:madeExecution ?observation .
+                            ?observation rdf:type hco:LatestObservation ;
+                                        saref:observes ?heatPump, ?temperatureSetpoint ;
+                                        saref:hasResult ?result .
+                            ?result rdf:type saref:PropertyValue ;
+                                    saref:isValueOfProperty ?temperatureSetpoint ;
+                                    saref:consistsOf ?room ;
+                                    saref:consistsOf ?temperatureResult .
+                            ?temperatureResult rdf:type saref:PropertyValue ;
+                                                saref:hasValue ?value ;
+                                                saref:isMeasuredIn ?unit .
+                        }`
+                });
 
-        fetchData();
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+
+                const data = await response.json();
+
+                console.log(data);
+            } catch (error) {
+                setError(error);
+            } finally {
+                setLoading(true);
+            }
+        }
+
+        const fetchCurrentOutsideTemp = async () => {
+            try {
+                const response = await fetch('https://hupie.northeurope.cloudapp.azure.com/hupie/query/?token=21129FCC24', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/sparql-query',
+                        'Accept': 'application/json'
+                    },
+                    body: `
+                        PREFIX hco: <https://www.tno.nl/building/ontology/heatpump-common-ontology#>
+                        PREFIX bot: <https://w3id.org/bot#>
+                        PREFIX om: <http://www.ontology-of-units-of-measure.org/resource/om-2/>
+                        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+                        PREFIX saref: <https://saref.etsi.org/core/>
+                        PREFIX saref4bldg: <https://saref.etsi.org/saref4bldg/> 
+                        
+                        SELECT ?id ?value ?unit
+                        WHERE {
+                            ?heatPump rdf:type hco:HeatPump ;
+                                        saref:hasIdentifier ?id .
+                            ?outside rdf:type bot:Zone, saref:FeatureOfInterest ;
+                                        saref:hasPropertyOfInterest ?currentTemperature ;
+                                        bot:hasBuilding ?building .
+                            ?building rdf:type saref4bldg:Building ;
+                                        saref4bldg:contains ?heatPump .
+                            ?currentTemperature rdf:type saref:PropertyOfInterest ;
+                                                saref:hasPropertyKind hco:CurrentTemperature .
+                            ?heatPump saref:madeExecution ?observation .
+                            ?observation rdf:type hco:LatestObservation ;
+                                        saref:observes ?outside, ?currentTemperature ;
+                                        saref:hasResult ?result .
+                            ?result rdf:type saref:PropertyValue ;
+                                    saref:isValueOfProperty ?currentTemperature ;
+                                    saref:hasValue ?value ;
+                                    saref:isMeasuredIn ?unit .
+                        }`
+                });
+
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+
+                const data = await response.json();
+
+                console.log(data);
+            } catch (error) {
+                setError(error);
+            } finally {
+                setLoading(true);
+            }
+        }
+        
+        const fetchCurrentState = async () => {
+            try {
+                const response = await fetch('https://hupie.northeurope.cloudapp.azure.com/hupie/query/?token=21129FCC24', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/sparql-query',
+                        'Accept': 'application/json'
+                    },
+                    body: 'PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX om: <http://www.ontology-of-units-of-measure.org/resource/om-2/> PREFIX hco: <https://www.tno.nl/building/ontology/heatpump-common-ontology#> PREFIX saref: <https://saref.etsi.org/core/> SELECT ?heatpump ?id ?state ?stateType ?startDateTime ?endDateTime WHERE { ?heatPump rdf:type hco:Heatpump ; saref:hasIdentifier ?id . ?state rdf:type ?stateType . ?heatPump saref:hasState ?state . ?state hco:hasStartTime ?startDateTime . ?state hco:hasEndTime ?endDateTime .  }'
+                });
+
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+
+                const data = await response.json();
+
+                console.log(data);
+            } catch (error) {
+                setError(error);
+            } finally {
+                setLoading(true);
+            }
+        }
+
+        fetchCurrentRoomTemp();
+        fetchRoomTempSetpoint();
+        fetchCurrentOutsideTemp();
+        fetchCurrentState();
     }, []);
 
     return (
         <>
             <div className="flex items-center justify-center">
                 <span>Test page</span>
+                <br />
+                <div>
+                    <p></p>
+                </div>
             </div>
         </>
     )
