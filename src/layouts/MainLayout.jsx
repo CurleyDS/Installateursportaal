b118de691a881a2cf3ab9f0fb4933050546c3fe9
Destiny Tiyjudy <149575130+Marvel-School@users.noreply.github.com>
2026-01-23 10:49:31 +0100

    feat: enhance layout for ultra-wide monitors and simplify MainLayout wrapper

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -144,8 +144,8 @@ function Main() {
 
             {/* Main Content Area */}
             <main className="flex-1 ml-64 overflow-x-hidden pt-20">
-                {/* Scaled Wrapper: Includes origin-top and safety padding */}
-                <div className="w-full mx-auto p-4 transition-transform duration-200 origin-top min-[2500px]:scale-[1.5] min-[2500px]:w-[66.666%] min-[3500px]:scale-[2.0] min-[3500px]:w-[50%]">
+                {/* Clean Wrapper: Percentage width, no scaling transforms */}
+                <div className="w-[95%] mx-auto p-4">
                     <Outlet context={{ search, filters }} />
                 </div>
             </main>
